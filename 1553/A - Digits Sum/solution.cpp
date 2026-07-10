50641d08bbed18790ee45a39312554716aa07e42
jay singh <jaysinghmd@gmail.com>
2026-07-10 19:59:02 +0530

    Add Digits Sum [A] from Codeforces

---
+++ 1553/A - Digits Sum/solution.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int w; cin>>w;
+   while(w--)
+   {
+       int n;cin>>n;
+       
+       cout<<(n+1)/10<<endl;
+   }
+ 
+    return 0;
+}
