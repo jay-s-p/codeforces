9a577e781857629af7ca6c776b1c9b9929c8d318
jay singh <jaysinghmd@gmail.com>
2026-07-10 20:05:02 +0530

    Add Skibidus and Amog'u [A] from Codeforces

---
+++ 2065/A - Skibidus and Amog'u/solution.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+     string s;cin>>s;
+     string t;
+     for(int i=0;i<s.size()-2;i++)
+     {
+         t=t+s[i];
+     }
+      t=t+'i';
+      cout<<t<<endl;
+ 
+       
+     
+        
+ 
+       
+}
+int main() {
+    int t;
+    cin >> t; 
+ 
+    while (t--) {
+     solve();  
+    }
+ 
+    return 0;
+}
