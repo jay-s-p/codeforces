641c39d009896f508f1705f2462a3cfa4f200e0f
jay singh <jaysinghmd@gmail.com>
2026-07-10 19:54:09 +0530

    Add Turtle and Good Strings [A] from Codeforces

---
+++ 2003/A - Turtle and Good Strings/solution.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int w;cin>>w;
+    while(w--){
+    int n;cin>>n;
+   string s;
+  cin>>s;
+ if(s.front()==s.back())
+  {
+      cout<<"NO"<<endl;
+  }
+else {cout<<"YES"<<endl;}}
+   
+    return 0;
+}
