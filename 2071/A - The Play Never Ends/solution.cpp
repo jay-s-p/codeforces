808869691beb8c5cab6453db8b28f83f082e5414
jay singh <jaysinghmd@gmail.com>
2026-07-10 20:05:07 +0530

    Add The Play Never Ends [A] from Codeforces

---
+++ 2071/A - The Play Never Ends/solution.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+  int n;cin>>n;
+  while(n--)
+  {
+      int m; cin>>m;
+      if(m%3==1)cout<<"YES"<<endl;
+      else cout<<"NO"<<endl;
+  }
+    return 0;
+}
