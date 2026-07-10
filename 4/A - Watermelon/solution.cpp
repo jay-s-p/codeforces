eed0da2a1e54c2c8f1fc31e5b5e1d8f3fc0bd4b9
jay singh <jaysinghmd@gmail.com>
2026-07-10 19:52:18 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+  int n; cin>>n;
+  if(n>2 && n%2==0)
+  {
+      cout<<"YES";
+  }
+  else cout<<"no";
+    return 0;
+}
