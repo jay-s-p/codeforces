acf1bd05ed23b35859bb85ff468a14cc9fd00f1c
jay singh <jaysinghmd@gmail.com>
2026-07-10 20:05:59 +0530

    Add Max and Mod [A] from Codeforces

---
+++ 2084/A - Max and Mod/solution.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define MOD 1000000007
+#define MOD1 998244353
+// #define int long long
+ 
+void jsr();
+void jsr()
+{
+    int n ;cin>>n;
+    if(n%2==0)
+    {
+        cout<<-1<<endl; return;
+    }
+    cout<<n<<" ";
+    for(int i=1; i<n; i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    
+ 
+ 
+ 
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+// jsr();
+    int qwemnb; cin>>qwemnb;
+    
+    while(qwemnb--)
+    {
+        jsr();
+    }
+ 
+    return 0;
+}
+ 
