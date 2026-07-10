fe9645b605c1ea0500b7b62ff37fd52643e0da1a
jay singh <jaysinghmd@gmail.com>
2026-07-10 19:55:09 +0530

    Add Gregor and Cryptography [A] from Codeforces

---
+++ 1549/A - Gregor and Cryptography/solution.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int w; cin>>w;
+    while(w--)
+    {
+        long long a;
+        cin>>a;
+        cout<<2<<" "<<a-1<<endl;
+    }
+    return 0;
+}
