328098e9cedc1d7edf0b52da1cfdeeef79d417b4
jay singh <jaysinghmd@gmail.com>
2026-07-10 19:51:58 +0530

    Add My First Sorting Problem [A] from Codeforces

---
+++ 1971/A - My First Sorting Problem/solution.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int n;
+//alskdjfhvb
+   cin>>n;
+   for(int i=0;i<n; i++)
+   {
+       int x,y;
+       cin>>y>>x;
+       if(x<=y)
+       {
+           cout<<x<<" "<<y<<endl;
+       }
+       else
+       {
+           cout<<y<<" "<<x<<endl;
+       }
+   }
+   
+   
+    return 0;
+}
