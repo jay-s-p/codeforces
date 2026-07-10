e7186a90286be7323b26e9e6058d940a21d2246d
jay singh <jaysinghmd@gmail.com>
2026-07-10 19:58:57 +0530

    Add Pretty Permutations [A] from Codeforces

---
+++ 1541/A - Pretty Permutations/solution.cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int w; cin>>w;
+   while(w--)
+   {
+       int n;cin>>n;
+       if(n==1)
+       {
+           cout<<2<<" "<<1<<endl;
+       }
+       else if(n==3)
+       {
+           cout<<3<<" "<<1<<" "<<2<<endl;
+       }
+       else if(n%2==0)
+       {
+           for(int i=1; i<=n; i+=2)
+           {
+               cout<<i+1<<" "<<i<<" ";
+           }
+           cout<<endl;
+       }
+       else if(n%2==1)
+       {
+           cout<<3<<" "<<1<<" "<<2<<" ";
+           for(int i=4; i<n;i+=2)
+           {
+               cout<<i+1<<" "<<i<<" ";
+           }
+           cout<<endl;
+       }
+   }
+ 
+    return 0;
+}
