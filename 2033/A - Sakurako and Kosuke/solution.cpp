d443346bbd8df7445ae0b25197dbaa3b76f5e4e4
jay singh <jaysinghmd@gmail.com>
2026-07-10 20:02:31 +0530

    Add Sakurako and Kosuke [A] from Codeforces

---
+++ 2033/A - Sakurako and Kosuke/solution.cpp
@@ -0,0 +1,21 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int w; cin>>w;
+  while(w--)
+  {
+      
+        int n; cin>>n;
+        if(n%2==0)
+        {
+            cout<<"Sakurako"<<endl;
+        }
+        else
+        {
+            cout<<"Kosuke"<<endl;
+        }
+  }
+ 
+    return 0;
+}
